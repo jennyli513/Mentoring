44b9ede7f0ae9cc65801719af84e6188fc94561a
jennyli513 <yanjie513@gmail.com>
2022-06-02 21:19:19 +1000

    mentor

--- Mentoring/Startup.cs
+++ Mentoring/Startup.cs
@@ -64,7 +64,7 @@ namespace Mentoring
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Subjects}/{action=Index}/{id?}");
             });
         }
     }
